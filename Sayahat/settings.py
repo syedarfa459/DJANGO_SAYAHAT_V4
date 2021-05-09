bce12030f600eef30ca7a63e589ad18c70fdeea3
Syed Muhammad Arfa <73629315+syedarfa459@users.noreply.github.com>
2021-05-10 04:41:49 +0500

    Update settings.py

--- Sayahat/settings.py
+++ Sayahat/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2dzlpti5vy#$c1@#jka2*iku=grf^y_im@2ya+k2(bwdh7azp5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
